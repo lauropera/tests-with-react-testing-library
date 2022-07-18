71725cc335d34d3f993be94c866f5c211fac85df
Lauro Pereira <lauropera01@gmail.com>
2022-07-18 15:54:49 -0300

    Completando o resto do req 1 que eu tinha esquecido

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from './helpers/renderWithRouter';
 import App from '../App';
 
@@ -22,5 +23,49 @@ describe('Checking App navbar', () => {
       name: 'Favorite Pokémons',
     });
     expect(favPkmnsLink).toBeInTheDocument();
+  });
+});
+
+describe('Navbar redirect links tests', () => {
+  it('Redirect to home page when the "Home" is clicked', () => {
+    const { history } = renderWithRouter(<App />);
+    const homeLink = screen.getByRole('link', { name: 'Home' });
+    expect(homeLink).toBeInTheDocument();
+
+    userEvent.click(homeLink);
+
+    const { location: { pathname } } = history;
+    expect(pathname).toBe('/');
+  });
+  it('Redirect to to about page when the "About" is clicked', () => {
+    const { history } = renderWithRouter(<App />);
+    const aboutLink = screen.getByRole('link', { name: 'About' });
+    expect(aboutLink).toBeInTheDocument();
+
+    userEvent.click(aboutLink);
+
+    const { location: { pathname } } = history;
+    expect(pathname).toBe('/about');
+  });
+  it('Redirect to favorites page when "Favorite Pokémons" is clicked', () => {
+    const { history } = renderWithRouter(<App />);
+    const favPkmnsLink = screen.getByRole('link', {
+      name: 'Favorite Pokémons',
+    });
+    expect(favPkmnsLink).toBeInTheDocument();
+
+    userEvent.click(favPkmnsLink);
+
+    const { location: { pathname } } = history;
+    expect(pathname).toBe('/favorites');
+  });
+  it('Redirect to a not found page when a random route is typed', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/aaaaaaaaaa');
+
+    const notFoundWarn = screen.getByRole('heading', {
+      name: /page requested not found/i,
+    });
+    expect(notFoundWarn).toBeInTheDocument();
   });
 });
