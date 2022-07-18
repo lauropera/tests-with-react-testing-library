a4df064772ca228eef75e6ead3690415bf3e60b8
Lauro Pereira <lauropera01@gmail.com>
2022-07-18 15:29:09 -0300

    Requisito 1 concluído

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,26 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './helpers/renderWithRouter';
+import App from '../App';
+
+describe('Checking App navbar', () => {
+  it('Expects to exist a "Home" link', () => {
+    renderWithRouter(<App />);
+    const homeLink = screen.getByRole('link', { name: 'Home' });
+    expect(homeLink).toBeInTheDocument();
+  });
+
+  it('Expects to exist an "About" link', () => {
+    renderWithRouter(<App />);
+    const aboutLink = screen.getByRole('link', { name: 'About' });
+    expect(aboutLink).toBeInTheDocument();
+  });
+
+  it('Expects to exist a "Favorite Pokémons" link', () => {
+    renderWithRouter(<App />);
+    const favPkmnsLink = screen.getByRole('link', {
+      name: 'Favorite Pokémons',
+    });
+    expect(favPkmnsLink).toBeInTheDocument();
+  });
+});
